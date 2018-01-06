ea73a4a40298e56ff1ec00198183bab62e546dc0
RubenTadeia <rubent03@gmail.com>
2018-01-06 04:31:20 +0000

    End of OA's Project

---
+++ OA_final_version/Group2_75189_75268_77063_79462/source_code/Part1/main_script.m
@@ -0,0 +1,149 @@
+%%  Project - Optimization and Algorithms
+%
+%   2017/2018
+%
+%   Luis Miguel Martins Alves       75189
+%
+%   Ruben Miguel Oliveira Tadeia    75268
+%
+%   Pedro Guilherme Moreira Falcao  77063
+%
+%   Diogo Henrique Monteiro Silva   79462
+%
+%   Script:
+%      -> First Phase
+%         - Part I
+
+%% Reset Variable states
+clc;
+clear all;
+close all;
+
+%% Loading the dataset into variables
+currentFolder = pwd;
+
+datasetFile = strcat(currentFolder,'/project5_gambling_dataset/dataset_project5_gambling.mat');
+
+load (datasetFile);
+
+%% Initialize figures and variables
+
+% Number of iterations
+iter = 100;
+trajectories = 1000;
+
+figure(1); clf;
+figure(2); clf;
+figure(3); clf;
+
+%% Solve optimization problem - Kelly
+cvx_begin quiet
+    variable b(N, 1)
+
+    % Build cost function
+    f = sum(prob_outcome_i' * log(returns_matrix * b));
+    maximize(f);
+
+    % Subject t
+    sum(b) == 1; 
+    b >= 0;
+cvx_end;
+
+%% Solve optimization problem - Risk Constraint Kelly
+alfa = 0.9;
+beta = 0.05;
+lambda_risk = log(beta)/log(alfa);
+
+cvx_begin quiet
+    variable b_rck(N, 1)
+
+    % build cost function
+    function_rck = sum(prob_outcome_i' * log(returns_matrix * b_rck));      
+    maximize(function_rck);
+
+    % subject to
+    risk_constraint = log(sum(exp(log(prob_outcome_i) - lambda_risk*log(returns_matrix * b_rck))));
+    risk_constraint <= 0;
+    sum(b_rck) == 1;
+    b_rck >= 0; 
+ cvx_end;
+ 
+%% Generating a random bets vector
+b_rnd = rand(20,1);
+b_rnd = b_rnd/sum(b_rnd);
+
+%% Generating the graphics
+
+% Init variables
+w = zeros(K,trajectories);
+w_rck = zeros(K,trajectories);
+w_rnd = zeros(K,trajectories);
+
+% For each trajectory
+for i = 1:trajectories
+    w(1,i) = 1;
+    w_rck(1,i) = 1;
+    w_rnd(1,i) = 1;
+
+    % For each time instant
+    for t = 2:iter
+        r = rand;
+        tmp1 = 0;
+        tmp2 = prob_outcome_i(1);
+        for j = 1:K           
+            if( r >= tmp1 && r <= tmp2)                
+                break; 
+            end
+            tmp1 = tmp1 + prob_outcome_i(j);
+            tmp2 = tmp2 + prob_outcome_i(j) + prob_outcome_i(j+1);
+        end  
+        w(t,i) = w(t-1,i) * (returns_matrix(j,:) * b);
+        w_rck(t,i) = w_rck(t-1,i) * (returns_matrix(j,:) * b_rck);
+        w_rnd(t,i) = w_rnd(t-1,i) * (returns_matrix(j,:) * b_rnd);
+    end
+    
+    if(rem(i,100) == 0)
+        figure(1); hold on;
+        plot(1:iter,w(:,i));
+        hold off;
+
+        figure(2); hold on;
+        plot(1:iter,w_rck(:,i));
+        hold off;   
+
+        figure(3); hold on;
+        plot(1:iter,w_rnd(:,i));
+        hold off;  
+    end
+end
+
+figure(1), xlabel('Trajectories'), ylabel('Wealth'), axis([0 100 0 700]);
+figure(2), xlabel('Trajectories'), ylabel('Wealth'), axis([0 100 0 10]);
+figure(3), xlabel('Trajectories'), ylabel('Wealth'), axis([0 100 0 10]);
+
+
+%% Final comparation plots
+w_mean = zeros(iter,1);
+w_rck_mean = zeros(iter,1);
+w_rnd_mean = zeros(iter,1);
+for row = 1 : iter
+	theSum = 0;
+    theSum_rck = 0;
+	theSum_rnd = 0;
+	for col = 1:trajectories
+		theSum = theSum + w(row, col);
+        theSum_rck = theSum_rck + w_rck(row, col);
+		theSum_rnd = theSum_rnd + w_rnd(row, col);
+    end
+    
+	% Now get the mean over all values in this row.
+	w_mean(row,1) = theSum / trajectories;
+    w_rck_mean(row,1) = theSum_rck / trajectories;
+    w_rnd_mean(row,1) = theSum_rnd / trajectories;
+end
+
+figure(4); hold on;
+plot(1:iter,w_mean,'r');
+plot(1:iter,w_rck_mean,'g');
+plot(1:iter,w_rnd_mean,'b');
+hold off;
---
+++ OA_final_version/Group2_75189_75268_77063_79462/source_code/Part2/part2_main.m
@@ -0,0 +1,70 @@
+%%  Project - Optimization and Algorithms
+%
+%   2017/2018
+%
+%   Luis Miguel Martins Alves       75189
+%
+%   Ruben Miguel Oliveira Tadeia    75268
+%
+%   Pedro Guilherme Moreira Falcao  77063
+%
+%   Diogo Henrique Monteiro Silva   79462
+%
+%   Script:
+%      -> Second Phase
+%         - Part I
+
+%% Reset Variable states
+clc;
+clear all;
+close all;
+
+%% Loading the dataset into variables
+currentFolder = pwd;
+
+datasetFile1 = strcat(currentFolder,'/project5_gambling_dataset/dataset_project5_gambling.mat');
+
+datasetFile2 = strcat(currentFolder,'/dataset_project5_phase2/dataset_project5_phase2.mat');
+
+load (datasetFile1);
+load (datasetFile2);
+
+%% Solve optimization problem - Kelly
+cvx_begin quiet
+    variable b(N, 1)
+    variable b_k
+    
+    % Build cost function
+    f1=1/2*((beta(1,1)*(b_k-b_ref(1,1)))+abs(beta(1,1)*(b_k-b_ref(1,1)))+(alfa(1,1)*(b_k-b_ref(1,1)))+abs(alfa(1,1)*(b_k-b_ref(1,1))));
+    f2=1/2*((beta(2,1)*(b_k-b_ref(2,1)))+abs(beta(2,1)*(b_k-b_ref(2,1)))+(alfa(2,1)*(b_k-b_ref(2,1)))+abs(alfa(2,1)*(b_k-b_ref(2,1))));
+    f3=1/2*((beta(3,1)*(b_k-b_ref(3,1)))+abs(beta(3,1)*(b_k-b_ref(3,1)))+(alfa(3,1)*(b_k-b_ref(3,1)))+abs(alfa(3,1)*(b_k-b_ref(3,1))));
+    f4=1/2*((beta(4,1)*(b_k-b_ref(4,1)))+abs(beta(4,1)*(b_k-b_ref(4,1)))+(alfa(4,1)*(b_k-b_ref(4,1)))+abs(alfa(4,1)*(b_k-b_ref(4,1))));
+    f5=1/2*((beta(5,1)*(b_k-b_ref(5,1)))+abs(beta(5,1)*(b_k-b_ref(5,1)))+(alfa(5,1)*(b_k-b_ref(5,1)))+abs(alfa(5,1)*(b_k-b_ref(5,1))));
+    f6=1/2*((beta(6,1)*(b_k-b_ref(6,1)))+abs(beta(6,1)*(b_k-b_ref(6,1)))+(alfa(6,1)*(b_k-b_ref(6,1)))+abs(alfa(6,1)*(b_k-b_ref(6,1))));
+    f7=1/2*((beta(7,1)*(b_k-b_ref(7,1)))+abs(beta(7,1)*(b_k-b_ref(7,1)))+(alfa(7,1)*(b_k-b_ref(7,1)))+abs(alfa(7,1)*(b_k-b_ref(7,1))));
+    f8=1/2*((beta(8,1)*(b_k-b_ref(8,1)))+abs(beta(8,1)*(b_k-b_ref(8,1)))+(alfa(8,1)*(b_k-b_ref(8,1)))+abs(alfa(8,1)*(b_k-b_ref(8,1))));
+    f9=1/2*((beta(9,1)*(b_k-b_ref(9,1)))+abs(beta(9,1)*(b_k-b_ref(9,1)))+(alfa(9,1)*(b_k-b_ref(9,1)))+abs(alfa(9,1)*(b_k-b_ref(9,1))));
+    f10=1/2*((beta(10,1)*(b_k-b_ref(10,1)))+abs(beta(10,1)*(b_k-b_ref(10,1)))+(alfa(10,1)*(b_k-b_ref(10,1)))+abs(alfa(10,1)*(b_k-b_ref(10,1))));
+    f11=1/2*((beta(11,1)*(b_k-b_ref(11,1)))+abs(beta(11,1)*(b_k-b_ref(11,1)))+(alfa(11,1)*(b_k-b_ref(11,1)))+abs(alfa(11,1)*(b_k-b_ref(11,1))));
+    f12=1/2*((beta(12,1)*(b_k-b_ref(12,1)))+abs(beta(12,1)*(b_k-b_ref(12,1)))+(alfa(12,1)*(b_k-b_ref(12,1)))+abs(alfa(12,1)*(b_k-b_ref(12,1))));
+    f13=1/2*((beta(13,1)*(b_k-b_ref(13,1)))+abs(beta(13,1)*(b_k-b_ref(13,1)))+(alfa(13,1)*(b_k-b_ref(13,1)))+abs(alfa(13,1)*(b_k-b_ref(13,1))));
+    f14=1/2*((beta(14,1)*(b_k-b_ref(14,1)))+abs(beta(14,1)*(b_k-b_ref(14,1)))+(alfa(14,1)*(b_k-b_ref(14,1)))+abs(alfa(14,1)*(b_k-b_ref(14,1))));
+    f15=1/2*((beta(15,1)*(b_k-b_ref(15,1)))+abs(beta(15,1)*(b_k-b_ref(15,1)))+(alfa(15,1)*(b_k-b_ref(15,1)))+abs(alfa(15,1)*(b_k-b_ref(15,1))));
+    f16=1/2*((beta(16,1)*(b_k-b_ref(16,1)))+abs(beta(16,1)*(b_k-b_ref(16,1)))+(alfa(16,1)*(b_k-b_ref(16,1)))+abs(alfa(16,1)*(b_k-b_ref(16,1))));
+    f17=1/2*((beta(17,1)*(b_k-b_ref(17,1)))+abs(beta(17,1)*(b_k-b_ref(17,1)))+(alfa(17,1)*(b_k-b_ref(17,1)))+abs(alfa(17,1)*(b_k-b_ref(17,1))));
+    f18=1/2*((beta(18,1)*(b_k-b_ref(18,1)))+abs(beta(18,1)*(b_k-b_ref(18,1)))+(alfa(18,1)*(b_k-b_ref(18,1)))+abs(alfa(18,1)*(b_k-b_ref(18,1))));
+    f19=1/2*((beta(19,1)*(b_k-b_ref(19,1)))+abs(beta(19,1)*(b_k-b_ref(19,1)))+(alfa(19,1)*(b_k-b_ref(19,1)))+abs(alfa(19,1)*(b_k-b_ref(19,1))));
+    f20=1/2*((beta(20,1)*(b_k-b_ref(20,1)))+abs(beta(20,1)*(b_k-b_ref(20,1)))+(alfa(20,1)*(b_k-b_ref(20,1)))+abs(alfa(20,1)*(b_k-b_ref(20,1))));
+    
+                    %From part 1
+    f = sum(prob_outcome_i' * log(returns_matrix * b))...
+    ...                % From part 2
+    -(f1+f2+f3+f4+f5+f6+f7+f8+f9+f10+f11+f12+f13+f14+f15+f16+f17+f18+f19+f20)...
+    ;
+    maximize(f);
+
+    % Subject t
+    sum(b) == 1; 
+    b >= 0;
+    b_k >= 0;
+cvx_end;
